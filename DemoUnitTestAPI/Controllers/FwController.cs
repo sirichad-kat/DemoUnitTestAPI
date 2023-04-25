47f03b5f9760141db467b5db89ba617d2bb04754
Sirichad.kat <sirichad.kat@cpf.co.th>
2023-04-25 21:59:39 +0700

    .

---
+++ DemoUnitTestAPI/Controllers/FwController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoUnitTestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FwController : ControllerBase
+    {
+    }
+}
